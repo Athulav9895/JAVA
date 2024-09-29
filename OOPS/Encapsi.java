13e88135e99e1d1987a3d2ccef11f64c0d1fb1a4
ATHUL A V <athulav2621@gmail.com>
2024-09-29 23:27:39 +0530

    Add files via upload

---
+++ OOPS/Encapsi.java
@@ -0,0 +1,13 @@
+package oops.com.in;
+
+public class Encapsi{
+	public static void main(String[]args){
+		Encapsulation obj=new Encapsulation();
+		obj.setA(15);
+		obj.setB(30);
+		System.out.println(obj.getA());
+		System.out.println(obj.getB());
+		
+	}
+
+}
