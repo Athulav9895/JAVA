3fde2cde5dbd109fb2a77fb634b6b6ed25ff5647
ATHUL A V <athulav2621@gmail.com>
2024-10-06 21:33:25 +0530

    Add files via upload

---
+++ UserEnterd.java
@@ -0,0 +1,15 @@
+package New.ja.va;
+
+import java.util.Scanner;
+
+public class UserEnterd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String:");
+		String n = sc.nextLine();
+		System.out.println("You entered: " + n);
+	}
+
+}
